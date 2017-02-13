77dd9d6065bea5be195b9cd53df6d9a4f09110ef
Gus Vine <a.vine@base2services.com>
2017-02-13 23:17:52 +1100

    direct-lvm storage options

--- recipes/default.rb
+++ recipes/default.rb
@@ -6,3 +6,6 @@
 #
 # All rights reserved - Do Not Redistribute
 #
+
+include_recipe "base2-docker::install"
+include_recipe "base2-docker::storage_direct_lvm"
---
+++ recipes/install.rb
@@ -0,0 +1,21 @@
+#
+# Cookbook Name:: base2-docker
+# Recipe:: install
+#
+# Copyright 2017, base2services
+#
+# All rights reserved - Do Not Redistribute
+#
+
+if node["platform"] == "amazon" && node["platform_version"] = "2016.09"
+  yum_package "docker" do
+    version node["base2-docker"]["docker_version"]["amazon"]["2016.09"]
+    action :install
+  end
+else
+  docker_installation_script "default"
+end
+
+service "docker" do
+  action :enable
+end
