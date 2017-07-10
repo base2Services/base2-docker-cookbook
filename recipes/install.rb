bdf892fe5ef8e537aeb1f3444bbb76f259e53d49
Zeyu Ye <z.ye@base2services.com>
2017-07-10 11:41:04 +1000

    ensure ipv4 forward is set to 1 in sysctl.conf

--- recipes/install.rb
+++ recipes/install.rb
@@ -7,6 +7,13 @@
 # All rights reserved - Do Not Redistribute
 #
 
+execute "ipv4_forward" do
+  command <<-EOF
+    sed -i -E "s/^ *net.ipv4.ip_forward += +.*$/net.ipv4.ip_forward = 1/g" /etc/sysctl.conf
+    sysctl -p
+  EOF
+end
+
 docker_service "default" do
   action [:create, :start]
 end
